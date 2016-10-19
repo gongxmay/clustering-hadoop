b547ec18692c56f8cd08b2f4947c6e75556fbdd4
Xuemei Gong <xgong@cis.drexel.edu>
2016-10-19 00:17:06 -0700

    Create repository

---
+++ src/lincs/drexel/edu/ap/APIteration.java
@@ -0,0 +1,111 @@
+package lincs.drexel.edu.ap;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FSDataInputStream;
+import org.apache.hadoop.fs.FSDataOutputStream;
+import org.apache.hadoop.fs.FileStatus;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.Mapper.Context;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+
+public class APIteration {	
+	public static int iterate (String path, String numOfDocs, double threshold, int numOfReducers, int numOfIters) throws Exception {	
+		int sameCount = 0;
+		boolean accumulatedSameFlag = false;		
+		ResponsibilityInitializer.initializeResponsibility(path,numOfDocs,numOfReducers);
+
+		int numOfIteration = 0;
+		int maxNumOfIteration = numOfIters;
+		double oldSim[][] = new double[Integer.parseInt(numOfDocs)][Integer.parseInt(numOfDocs)];
+		double oldResp[][] = new double[Integer.parseInt(numOfDocs)][Integer.parseInt(numOfDocs)];
+		double oldAvail[][] = new double[Integer.parseInt(numOfDocs)][Integer.parseInt(numOfDocs)];
+		double newSim[][] = new double[Integer.parseInt(numOfDocs)][Integer.parseInt(numOfDocs)];
+		double newResp[][] = new double[Integer.parseInt(numOfDocs)][Integer.parseInt(numOfDocs)];
+		double newAvail[][] = new double[Integer.parseInt(numOfDocs)][Integer.parseInt(numOfDocs)];
+		for (int doc1 = 0; doc1 < Integer.parseInt(numOfDocs); doc1++) {
+			for (int doc2 = 0; doc2 < Integer.parseInt(numOfDocs); doc2++) {
+				oldSim[doc1][doc2] = 0d;
+				oldResp[doc1][doc2] = 0d;
+				oldAvail[doc1][doc2] = 0d;
+			}
+		}
+
+		//Iterate until converges or after 100 iterations.
+		for (int i=0; i < maxNumOfIteration; i++) {
+			numOfIteration = i + 1;
+			AvailibilityCalculator.updateAvailibility(path, numOfDocs, numOfReducers);	
+			Configuration conf = new Configuration();
+			ResponsibilityCalculator.updateResponsibility(path, numOfDocs, numOfReducers);
+			conf.addResource(new Path("/etc/hadoop/conf/core-site.xml"));
+			conf.addResource(new Path("/etc/hadoop/conf/hdfs-site.xml"));
+
+			FileSystem fileSystemIn = FileSystem.get(conf);
+			FileStatus[] status = fileSystemIn.listStatus(new Path(path+"/Similarity"));
+			for (int fileId = 0; fileId < status.length; fileId++) {
+				if (!status[fileId].getPath().toString().contains("part"))
+					continue;
+				FSDataInputStream br = fileSystemIn.open(status[fileId].getPath());
+				String strLine = "";
+				while ((strLine = br.readLine()) != null) {
+					String line[] = strLine.split("\t");
+					int row = Integer.parseInt(line[0].split(" and ")[0]);
+					int colum = Integer.parseInt(line[0].split(" and ")[1]);
+					newSim[row-1][colum-1] = Double.parseDouble(line[1].split(",")[0]);
+					newResp[row-1][colum-1] = Double.parseDouble(line[1].split(",")[1]);
+					newAvail[row-1][colum-1] = Double.parseDouble(line[1].split(",")[2]);
+				}
+			}
+
+			boolean sameFlag = true;
+			for (int doc1 = 0; doc1 < Integer.parseInt(numOfDocs); doc1++) {
+				for (int doc2 = 0; doc2 < Integer.parseInt(numOfDocs); doc2++) {
+					if (Math.abs((newAvail[doc1][doc2]+newResp[doc1][doc2])-(oldAvail[doc1][doc2]+oldResp[doc1][doc2])) > threshold) 
+						sameFlag=false;
+				}
+			}
+			if (sameFlag && !accumulatedSameFlag) {
+				sameCount = 1; 
+				accumulatedSameFlag = true;
+			} else if (sameFlag && accumulatedSameFlag) {
+				sameCount ++;
+			} else if (!sameFlag  && accumulatedSameFlag) {
+				accumulatedSameFlag = false;
+				sameCount = 0;
+			}
+			//if the evidence doesn't change for 10 iterations, break the loop.
+			if (sameCount == 10)
+				break;
+			for (int doc1 = 0; doc1 < Integer.parseInt(numOfDocs); doc1++){
+				for(int doc2 = 0; doc2 < Integer.parseInt(numOfDocs); doc2++){
+					oldSim[doc1][doc2] = newSim[doc1][doc2];
+					oldResp[doc1][doc2] = newResp[doc1][doc2];
+					oldAvail[doc1][doc2] = newAvail[doc1][doc2];
+				}
+			} 
+		}
+		return 1;
+	}
+}
---
+++ src/lincs/drexel/edu/cosine/TfIdf.java
@@ -0,0 +1,88 @@
+package lincs.drexel.edu.cosine;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+
+public class TfIdf {
+
+	public static class TF_IDFMapper extends Mapper<LongWritable, Text, Text, Text> {
+		
+		public void map(LongWritable key, Text value, Context context)throws IOException, InterruptedException {
+			String[] wordWithDoc = value.toString().split("@");
+			String[] docWithCount = wordWithDoc[1].split("\t");
+			Text word = new Text(wordWithDoc[0]);
+			Text text =new Text(docWithCount[0] + "=" + docWithCount[1]);
+			context.write(word, text);
+		}	
+	}
+	
+	public static class TF_IDFReducer extends Reducer<Text, Text, Text, Text> {
+		
+		public void reduce(Text key, Iterable<Text> value, Context context) throws IOException,InterruptedException {
+			Configuration conf = context.getConfiguration();
+			int numOfDocs = Integer.parseInt(conf.get("numOfDocs"));
+			int wordCountInCorpus=0;
+			Map<String, Double> map=new HashMap<String, Double>();
+			
+			for (Text val : value) {
+				wordCountInCorpus++;
+				String[] docWithTf = val.toString().split("=");
+				String[]nOverN=docWithTf[1].split("/");
+				double WordCountOverWordsPerDoc=Double.parseDouble(nOverN[0].trim())/Double.parseDouble(nOverN[1].trim());
+				map.put(docWithTf[0], WordCountOverWordsPerDoc);				
+			}
+			//Calculate tf*idf
+			for (String document : map.keySet()) {
+				double tf = map.get(document);
+				double preIdf = (double) numOfDocs / (double) wordCountInCorpus;
+				double idf= Math.log10(preIdf);
+				double tfIdf = idf != 0 ? tf*idf : tf;
+				context.write(new Text(key+"@"+document),new Text(String.valueOf(tfIdf)));
+			}
+		}
+	}
+	
+	public static void calculateTfIdf(String path, int numOfDocs)throws Exception {
+		Configuration conf = new Configuration();
+		conf.set("numOfDocs",numOfDocs);
+	    Job job = new Job(conf, "TfIdf");
+	    job.setJarByClass(TfIdf.class);
+
+	    job.setOutputKeyClass(Text.class);
+	    job.setOutputValueClass(Text.class);
+	    
+	    //Setting the mapper and reducer classes
+	    job.setMapperClass(TF_IDFMapper.class);
+	    job.setReducerClass(TF_IDFReducer.class);
+	    
+	    //Setting the type of input format.
+	    job.setInputFormatClass(TextInputFormat.class);
+	    job.setOutputFormatClass(TextOutputFormat.class);
+	    
+	    Path input=new Path(path+"/unique");
+	    Path output=new Path(path+"/TF_IDF");
+	    
+	    FileSystem fs = FileSystem.get(conf);
+	    if (fs.exists(output))
+	    	fs.delete(output, true);
+	    
+	    FileInputFormat.addInputPath(job, input);
+	    FileOutputFormat.setOutputPath(job, output);
+	        
+	    job.waitForCompletion(true);
+	}
+}
